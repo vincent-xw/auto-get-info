244c4862889015385a329a61e8e3349c2b31a46d
vincent-xw <jiaxuewen2006@163.com>
2017-08-23 19:23:17 +0800

    百度地图接口接入成功，但是request依然抽风

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const app = express()
 app.get('/getInfo', function (req, res) {
 	const request = require('request');
 	var str = req.query.name;
-	request("https://bj.lianjia.com/chengjiao/rs"+str+"/", function (error, response, body) {
-	  let result = {
-			'status':true,
-			'data':body
-		};
+	// http://map.baidu.com/?qt=s&wd="+str
+	// "https://bj.lianjia.com/chengjiao/rs"+str+"/"
+	var url = "http://api.map.baidu.com/place/v2/search?query="+str+"&region=北京&output=json&ak=m1YPA069wCs1pqMusm0ZYgVlnKChbWMz";
+	request(url, function (error, response, body) {
+		let result;
+		if(!error && response && response.statusCode == 200){
+			result = {
+				'status':true,
+				'data':JSON.parse(body)
+			};
+		}else{
+			result = {
+				'status':false,
+				'msg':response.statusCode
+			}
+		}
+	  
 	  res.send(result);
 	});
-	
-	
+
+
 });
 
 app.get('*', function(req, res){
