56044dce401af8eb0d21b4c38d0c1ce39f0a4dd7
vincent-xw <jiaxuewen2006@163.com>
2017-08-27 19:43:13 +0800

    add configs

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 const express = require('express');
 const app = express();
 // 加载hbs配置
-require("./config/hbs_config");
+var hbs_config = require("./config/hbs_config");
 
-app.get('/getInfo', function (req, res) {
-	const request = require('request');
-	var str = req.query.name;
-	// http://map.baidu.com/?qt=s&wd="+str
-	// "https://bj.lianjia.com/chengjiao/rs"+str+"/"
-	var url = encodeURI("http://api.map.baidu.com/place/v2/search?query="+str+"&region=北京&output=json&ak=m1YPA069wCs1pqMusm0ZYgVlnKChbWMz");
-	request(url, function (error,response,body) {
-		let result;
-		if(!error && response && response.statusCode == 200){
-			result = {
-				'status':true,
-				'data':JSON.parse(body)
-			};
-		}else{
-			result = {
-				'status':false,
-				'msg':response.statusCode
-			}
-		}
-	  
-	  res.send(result);
-	});
+hbs_config(app,express);
 
+// 加载接口配置
+let interface_config = require("./config/interface");
 
-});
-app.get('/', function(req, res){
-  res.send("./dashboard/index.html");
-});
-
-app.get('*', function(req, res){
-  res.json({'status':404});
-});
+interface_config(app);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
---
+++ config/interface.js
@@ -0,0 +1,32 @@
+module.exports = function(app){
+    app.get('/getInfo', function (req, res) {
+        const request = require('request');
+        var str = req.query.name;
+        var url = encodeURI("http://api.map.baidu.com/place/v2/search?query="+str+"&region=北京&output=json&ak=m1YPA069wCs1pqMusm0ZYgVlnKChbWMz");
+        request(url, function (error,response,body) {
+            let result;
+            if(!error && response && response.statusCode == 200){
+                result = {
+                    'status':true,
+                    'data':JSON.parse(body)
+                };
+            }else{
+                result = {
+                    'status':false,
+                    'msg':response.statusCode
+                }
+            }
+        
+        res.send(result);
+        });
+
+
+    });
+    app.get('/', function(req, res){
+        res.render("/dashboard/index");
+    });
+
+    app.get('*', function(req, res){
+    res.json({'status':404});
+    });
+}
