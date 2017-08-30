cb3127940cf6ac7bc149a59086ae386a849fb4ce
vincent-xw <jiaxuewen2006@163.com>
2017-08-30 20:11:29 +0800

    二期修改

--- dashboard/web/angularJS/maincontroller.js
+++ dashboard/web/angularJS/maincontroller.js
@@ -28,18 +28,33 @@ app.controller("main",["$scope","$http",function($scope,$http){
     }
     // 百度地图API功能
     var bmap = new BMap.Map("baidu");          
-    bmap.centerAndZoom(new BMap.Point(116.404, 39.915), 13);
+    bmap.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
     var local = new BMap.LocalSearch(bmap, {
         renderOptions:{map: bmap},
         onSearchComplete: function(results){
-			// 判断状态是否正确
-			if (local.getStatus() == BMAP_STATUS_SUCCESS){
-				$scope.listInfo(results,"baidu");
-			}
 		}
     });
     bmap.enableScrollWheelZoom(true);
-
+    var geoc = new BMap.Geocoder();  
+	bmap.addEventListener("click", function(e){        
+        var pt = e.point;
+        $scope.loading = true;
+		geoc.getLocation(pt, function(rs){
+            var addComp = rs.addressComponents;
+            $scope.$apply(function(){
+                $scope.loading = false;
+                for(var i = 0; i < rs.surroundingPois.length; i++){
+                    if(rs.surroundingPois[i].Si == "房地产"){
+                        $scope.mapDistrict = rs.surroundingPois[i].title.split("小区")[0];
+                        $scope.detailAddress = rs.surroundingPois[i].address;
+                        return;
+                    }
+                }
+                $scope.mapDistrict = "没有获取到有效的小区信息";
+                $scope.detailAddress = "";
+            });
+		});        
+	});
    // 高德地图api
    var amap = new AMap.Map('gaode',{
         resizeEnable: true,
@@ -102,69 +117,18 @@ app.controller("main",["$scope","$http",function($scope,$http){
         arr = [];
         // 高德搜索
         placeSearch.search($scope.key, function(status, result) {
-            if(status == "complete"){
-                $scope.listInfo(result,"gaode");
-            }
+            
         });
         // 百度搜索
         local.search($scope.key);
     }
-    // 给出搜索结果建议
-    $scope.show = false;
-    var arr = [];
-    $scope.listInfo = function(result,str){
-        $scope.index++;
-        
-        if(str == "baidu"){
-            console.log(result);
-            for(var i = 0; i < result.vr.length; i++){
-                if(result.vr[i].tags && result.vr[i].tags.length != 0){
-                    for(var j = 0; j < result.vr[i].tags.length; j++){
-                        if(result.vr[i].tags[j].indexOf("小区") != -1){
-                            var temp = {
-                                source:str,
-                                name:result.vr[i].title.split("-")[0]
-                            };
-                            arr.push(temp);
-                            break;
-                        }
-                    }
-                }
-                
-            }
-        }else{
-            console.log(result);
-            for(var i = 0; i < result.poiList.pois.length; i++){
-                if(result.poiList.pois[i].type != undefined){
-                    if(result.poiList.pois[i].type.indexOf("住宅小区") != -1){
-                        var temp = {
-                            source:str,
-                            name:result.poiList.pois[i].name
-                        };
-                        arr.push(temp);
-                    }
-                }
-            }
-        }
-        if($scope.index == 2){
-            $scope.$apply(function(){
-                $scope.show = true;
-                arr = arr.unique('source');
-                $scope.suggest = arr;
-                $scope.showSuggest = true;
-                console.log(arr);
-            });
-            var timer = null;
-            timer = setTimeout(function(){
-                $scope.$apply(function(){
-                    $scope.show = false;
-                });
-            },1000);
-        }
-    }
 
     // 检索小区信息
+    $scope.searchPointBefore = function(str){
+        
+    }
     $scope.searchDistrict = function(str){
+        $scope.switch("panel","districtPanel");
         var name = str || $scope.district;
         $scope.districtName = name;
         $http.get("/getDistrictSaleInfo?name="+name).then(
